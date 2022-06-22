318cb3fcfd73e1a0493a62d7ef82b716eb1b1bf2
Nokeen <cordrepayne@gmail.com>
2022-06-22 13:14:37 -0500

    Add music, new zombie type, zombie health, zombie rotating, menus, mute/unmute button.

--- js/index.js
+++ js/index.js
@@ -63,7 +63,11 @@ class BulletObject{
     this.y += this.aim.y;
   }
 }
-
+const menuMusic = new Audio('/music/menusongdark.mp3')
+const gameMusic = new Audio('/music/thunder.mp3')
+gameMusic.loop = true;
+gameMusic.volume = .6;
+menuMusic.loop = true;
 window.onload = () => {
   //create a starting frame count of zero
   let totalFrameCount = 0;
@@ -72,8 +76,7 @@ window.onload = () => {
   //will contain all of bullets
   let bulletArray = [];
   let ammoArray = [1, 1, 1, 1, 1, 1];
-  //initializes interval id to be used as a timer later
-  let intervalId = null;
+  // let intervalId = null;
   function reset(){
     setTimeout(() => {
       const myCanvas = document.querySelector('canvas');
@@ -84,15 +87,70 @@ window.onload = () => {
       zombieArray = [];
       bulletArray = [];
       ammoArray = [1, 1, 1, 1, 1, 1];
+      starter = false;
+      gameMusic.muted = true;
     }, 5000)
   }
-  //starts game when you click the start button
+  //button functionality
   const menu = document.getElementById('main-menu');
+  const controls = document.getElementById('controls-screen');
+  const help = document.getElementById('help-screen');
+  const mute = document.querySelector('#mutetoggle');
+  var starter = false;
+  mute.onclick = () =>{
+    if(mute.innerHTML == 'Unmuted'){
+      if(!starter){
+        menuMusic.muted = true;
+      }else{
+        gameMusic.muted = true;
+      }
+      mute.innerHTML = 'Muted'
+    }else{
+      if(!starter){
+        menuMusic.muted = false;
+        menuMusic.play();
+      }else{
+        gameMusic.muted = false;
+        gameMusic.play();
+      }
+      mute.innerHTML = 'Unmuted'
+    }
+  }
   document.getElementById('start-button').onclick = () => {
+    starter = true;
+    menuMusic.muted = true;
+    if(mute.innerHTML == 'Unmuted'){
+      gameMusic.play();
+    }
     setTimeout(() =>{
       menu.style.visibility = 'hidden';
+      document.getElementById('round').style.visibility = 'visible';
       startGame();
     },500)
+  };
+  document.getElementById('controls').onclick = () => {
+    setTimeout(() =>{
+      controls.style.visibility = 'visible'
+      menu.style.visibility = 'hidden';
+    },100)
+  };
+  document.getElementById('help').onclick = () => {
+    setTimeout(() =>{
+      help.style.visibility = 'visible'
+      menu.style.visibility = 'hidden';
+    },100)
+  };
+  document.getElementById('back1').onclick = () => {
+    setTimeout(() =>{
+      menu.style.visibility = 'visible';
+      controls.style.visibility = 'hidden';
+    },100)
+  };
+  document.getElementById('back2').onclick = () => {
+    setTimeout(() =>{
+      menu.style.visibility = 'visible';
+      help.style.visibility = 'hidden';
+    },100)
   };
   //game loop
   function startGame() {    
@@ -109,31 +167,34 @@ window.onload = () => {
     const player = new CharacterObject(myCanvas.width/2, myCanvas.height/2, myCanvas.width/20, myCanvas.width/20, ctx, 'red');
     //zombie class references player
     class ZombieObject extends CharacterObject{
-      constructor(x, y, width, height, canvasContext, color, type){
+      constructor(x, y, width, height, canvasContext, color, type, image, health){
         super(x, y, width, height, canvasContext, color);
         this.type = type;
+        this.image = new Image();
+        this.image.src = image;
+        this.health = health;
       }
-      
+      draw(){
+        ctx.save();
+        let angle = Math.atan2((player.y+(player.height/2)) - (this.y+(this.height/2)), (player.x+(player.width/2)) - (this.x+(this.width/2)))
+        let cx = this.x + 0.5 * this.width
+        let cy = this.y + 0.5 * this.height
+        ctx.translate(cx, cy)
+        ctx.rotate(angle+359.7)
+        ctx.translate(-cx, -cy)
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.restore();
+      }
       //moves zombies towards player's current position
       update(){
-        let speed;
-        this.draw();
+       
         let angle = Math.atan2(player.y - this.y, player.x - this.x)
-        switch(this.type){
-          case 0:
-            speed = {
-              x: Math.cos(angle) * .2,
-              y: Math.sin(angle) * .2
-            };
-            break;
-          case 1:
-            speed = {
-              x: Math.cos(angle) * .6,
-              y: Math.sin(angle) * .6
-            };
-            break;
-        }
-        
+        this.image.style.transform = `rotate(90deg)`
+        this.draw();
+        let speed = {
+          x: Math.cos(angle) * this.type,
+          y: Math.sin(angle) * this.type
+        };
         this.x += speed.x;
         this.y += speed.y;
       }
@@ -145,18 +206,16 @@ window.onload = () => {
       }
     }
 
-
     let roundCurrent = 1;
     let roundText = document.querySelector("#round span");
-
     let difficulty = 1;
-    let zombiesAlive = zombieArray.length;
-
-    let frames;
+    var frames;
     let score = 0;
     let points = document.querySelector("#score span");
     let ammunition = document.querySelector("#ammo span");
 
+    const greenColor = 'green'
+    const yellowColor = 'yellow'
 
     function updateGame(){
       frames = totalFrameCount++;
@@ -179,25 +238,16 @@ window.onload = () => {
           x = Math.random() * myCanvas.width;
           y = Math.random() < 0.5 ? 0 - player.height: myCanvas.height + player.height;
         }
-        const greenColor = 'green'
-        const yellowColor = 'yellow'
-        // let angle = Math.atan2(player.y - y, player.x - x)
-        // const greenVelocity = {
-        //   x: Math.cos(angle) * 1,
-        //   y: Math.sin(angle) * 1
-        // }
-        // const yellowVelocity = {
-        //   x: Math.cos(angle) * 5,
-        //   y: Math.sin(angle) * 5
-        // }
         if(Math.random() < (0.03*roundCurrent)){
-          zombieArray.push(new ZombieObject(x, y, player.width, player.height, ctx, yellowColor, 1))
+          zombieArray.push(new ZombieObject(x, y, 40, 50, ctx, yellowColor, .4, "/images/yellowzombie.png", 2))
         }else{
-          zombieArray.push(new ZombieObject(x, y, player.width, player.height, ctx, greenColor, 0))
+          zombieArray.push(new ZombieObject(x, y, 40, 50, ctx, greenColor, .2, "/images/greenzombie.png", 1))
         }
-       
-
+        if(totalFrameCount % (20000) === 0){
+          zombieArray.push(new ZombieObject(x, y, 120, 150, ctx, greenColor, .1, "/images/redzombie.png", 5))
+        }
       }
+
       zombieArray.forEach((zombie, index) => {
         zombie.update()
         //zombie and player collision
@@ -216,7 +266,10 @@ window.onload = () => {
           if(distBetween-zombie.width/2-bullet.radius/2<1){
             //fixes jitter frames when deleting zombies
             setTimeout(() =>{
-              zombieArray.splice(index, 1);
+              zombieArray[index].health -= 1;
+              if(zombieArray[index].health < 1){
+                zombieArray.splice(index, 1);
+              }
             }, 0)
             bulletArray.splice(bulletIndex, 1);
             score+=10;
@@ -241,7 +294,6 @@ window.onload = () => {
         //moves zombies
       }
     }
-
     
     //only start the game loop after the road image has finished loading
     floorImg.onload =  () => {
@@ -293,28 +345,6 @@ window.onload = () => {
           break;
       }
     });
-    //gets direction of mouse from player
-    myCanvas.onmousemove = (e) => {
-      // //targets the canvas inside of screen 
-      // const bnds = e.target.getBoundingClientRect();
-      // //gets x,y position of mouse inside canvas
-      // const x = Math.floor(e.clientX - bnds.left)
-      // const y = e.clientY - bnds.top;
-      // //gets the position of the player
-      // const w = player.x+48
-      // const h = player.y+48
-      // //gets distance between mouse and player
-      // const vx = x-w
-      // const vy = y-h
-      // //gets mouse position in radians
-      // const rad = Math.atan2(vy, vx)
-      // //convert radians to degrees and round it
-      // let deg = Math.round(rad *(180 / Math.PI));
-      // //convert to 360 degrees
-      // if (deg < 0){deg = (deg + 360) % 360}
-      // // console.log(`Current Degree: ${deg}`);
-      // console.log(`X:${e.target.getBoundingClientRect().clientX} | X2:${x}`);
-    }
     myCanvas.onclick = (e) => {
       //gets angle between player and mouse
       const angle = Math.atan2((e.clientY-e.target.getBoundingClientRect().top)-(player.y+(player.height/2)), 
